3cf05b7ef7ae9ebe51e1ca1120de05b003db4e21
Marco Gregori <marco.gregori74@gmail.com>
2022-12-02 16:31:36 -0300

    Refactor del input

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/themes/default.dart';
 
 import '../widgets/widgets.dart';
 
@@ -12,6 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
+
   final _formKey = GlobalKey<FormState>();
   String _server = 'LAS';
 
@@ -32,24 +34,33 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const SizedBox(height: 60),
             const FadeInImage(
-              image: AssetImage('assets/images/cofre-hextech.png'),
+              image: AssetImage('assets/images/LogoDark.png'),
               placeholder: AssetImage('assets/images/background.png'),
               fit: BoxFit.fill,
               height: 170,
             ),
-            const SizedBox(height: 30),
-            const Text('¡Esos cofrecitos no se van a sacar solos!',
+            const SizedBox(height: 40),
+            const Padding(padding: EdgeInsets.fromLTRB(30, 0, 30, 10),
+            child:(Text('¡Esos cofrecitos no se van a sacar solos!',
                 style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 25,
                     fontWeight: FontWeight.w800,
                     fontFamily: 'Beaufort',
-                    color: Colors.black87)),
+                    color: Color.fromARGB(255, 217, 219, 209))))
+            ),
             const SizedBox(height: 5),
-            const Text(
+            const Padding(padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+            child: Text(
               'Confirma si tienes cofre disponible para tus partidas de ARAM.',
-              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
-              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromARGB(255, 217, 219, 209),
+                fontSize: 13, 
+                fontWeight: FontWeight.w400,
+              ),
+              textAlign: TextAlign.left,
+            ),
             ),
             Form(
                 key: _formKey,
@@ -57,13 +68,25 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Row(
+                    Padding(padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    child: Row(
                       children: [
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            DropdownButton(
+                            Container(
+                              padding: const EdgeInsets.only(left: 20,top: 2),
+                              decoration: const BoxDecoration(
+                                color:Color.fromARGB(255, 7, 13, 15),
+                                borderRadius: BorderRadius.only( topLeft: Radius.circular(40),
+                                bottomLeft: Radius.circular(40)
+                                )
+                              ),
+                              height: 55,
+                              width: 80,
+                              child: DropdownButton(
+                                underline: const SizedBox(),
                                 value: _server,
                                 icon: const Icon(Icons.keyboard_arrow_down),
                                 items: _servers.map((String server) {
@@ -75,6 +98,8 @@ class _HomePageState extends State<HomePage> {
                                     _server = selected!;
                                   });
                                 }),
+                                )
+                                ,
                           ],
                         ),
                         Column(
@@ -82,21 +107,31 @@ class _HomePageState extends State<HomePage> {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             Container(
-                              width: 300,
+                              padding: const EdgeInsets.only(left: 5,top: 2),
+                              color: const Color.fromARGB(255, 7, 13, 15),
+                              height: 55,
+                              width: 160,
                               child: TextFormField(
                                 decoration: const InputDecoration(
-                                    icon: Icon(Icons.arrow_forward_ios),
-                                    labelText: 'Summoner id'),
+                                    border: InputBorder.none,
+                                    hintText: 'Summoner id'),
                               ),
                             ),
                           ],
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Column(children: <Widget>[
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
                           ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(0, 55),
+                              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(40),
+                                bottomRight: Radius.circular(40),
+                                )),
+                              backgroundColor: const Color.fromRGBO(174, 145, 75, 1),
+                            ),
                             onPressed: () {
                               // Validate returns true if the form is valid, or false otherwise.
                               if (_formKey.currentState!.validate()) {
@@ -110,8 +145,10 @@ class _HomePageState extends State<HomePage> {
                             },
                             child: const Text('Submit'),
                           ),
-                        ]),
+                        ])
                       ],
+                      
+                    ),
                     ),
                   ],
                 ))
