a8751165652987830e301d1b00265a97cb699624
Dante Pereyra <pereyra.dan@gmail.com>
2022-12-05 19:12:12 -0300

    merge fix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -69,7 +69,6 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-<<<<<<< HEAD
                     Padding(
                       padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
                       child: Row(
@@ -153,89 +152,6 @@ class _HomePageState extends State<HomePage> {
                               ])
                         ],
                       ),
-=======
-                    Padding(padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                    child: Row(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.only(left: 20,top: 2),
-                              decoration: const BoxDecoration(
-                                color:Color.fromARGB(255, 7, 13, 15),
-                                borderRadius: BorderRadius.only( topLeft: Radius.circular(40),
-                                bottomLeft: Radius.circular(40)
-                                )
-                              ),
-                              height: 55,
-                              width: 80,
-                              child: DropdownButton(
-                                underline: const SizedBox(),
-                                value: _server,
-                                icon: const Icon(Icons.keyboard_arrow_down),
-                                items: _servers.map((String server) {
-                                  return DropdownMenuItem(
-                                      value: server, child: Text(server));
-                                }).toList(),
-                                onChanged: (String? selected) {
-                                  setState(() {
-                                    _server = selected!;
-                                  });
-                                }),
-                                )
-                                ,
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.only(left: 5,top: 2),
-                              color: const Color.fromARGB(255, 7, 13, 15),
-                              height: 55,
-                              width: 160,
-                              child: TextFormField(
-                                decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: 'Summoner id'),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: const Size(0, 55),
-                              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(40),
-                                bottomRight: Radius.circular(40),
-                                )),
-                              backgroundColor: const Color.fromRGBO(174, 145, 75, 1),
-                            ),
-                            onPressed: () {
-                              // Validate returns true if the form is valid, or false otherwise.
-                              if (_formKey.currentState!.validate()) {
-                                // If the form is valid, display a snackbar. In the real world,
-                                // you'd often call a server or save the information in a database.
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text('Processing Data')),
-                                );
-                              }
-                            },
-                            child: const Text('Submit'),
-                          ),
-                        ])
-                      ],
-                      
-                    ),
->>>>>>> cd657c5ae7bcd363296bd7352bd381c382086418
                     ),
                   ],
                 ))
